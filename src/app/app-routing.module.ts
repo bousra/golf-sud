b6b9489483b0c6e07151b14a87fa6b2b24debf11
infra <infra@gitechsn.com>
2022-05-14 20:48:13 +0000

    bilan et diagnostic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { PresentationComponent } from './presentation/presentation.component';
-
+import {BilanDiagnosticComponent} from './bilan-diagnostic/bilan-diagnostic.component'
 export const routes: Routes = [
 
   { path: '', component: PresentationComponent },
   { path: 'accueil', component: PresentationComponent },
-  { path: 'bilan', component: PresentationComponent },
+  { path: 'bilan', component: BilanDiagnosticComponent },
 ];
